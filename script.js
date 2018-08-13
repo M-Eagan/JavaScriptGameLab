7dd1758046e1bfb53d7fe6aa23d36fc347bdb52c
Megan-Eagan <32804934+M-Eagan@users.noreply.github.com>
2018-08-13 16:35:02 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 let playQuestion = prompt("Would you like to play the game with 2 players?");
-let userName = prompt("What is your name?");
-
 
 let userWins = 0;
 let userHealthPoints= 40;
 let grantHealthPoints= 10;
 
+if (playQuestion === "Yes" || playQuestion === "Yeah" || playQuestion === "yes" || playQuestion === "yeah") {
 
-function getRandomInt() {
-  return Math.floor(Math.random() * 2) + 1;
+let userName = prompt("What is your name?");
+
+function getDamage() {
+  return Math.floor(Math.random() * 5) + 1;
 }
 
-function randomGame() {
-while(userWins < 3 && userHealthPoints > 0) {
-  userHealthPoints -= getRandomInt();
+startCombat();
+
+function startCombat() {
+while(playQuestion === "Yes" || playQuestion === "Yeah" || playQuestion === "yes" || playQuestion === "yeah") {
+  
+if (userWins < 3 && userHealthPoints > 0) {
+  userHealthPoints -= getDamage();
   console.log(userName +" has "+ userHealthPoints +" health left.");
-  grantHealthPoints -= getRandomInt();
-  console.log("Grant the Mighty Chicken has "+ grantHealthPoints + " left.")
+  grantHealthPoints -= getDamage();
+  console.log("Grant the Mighty Chicken has "+ grantHealthPoints + " left.");
 
   if(grantHealthPoints <= 0) {
     userWins++;
     grantHealthPoints = 10;
   }
-}
+
+
 if (userHealthPoints <= 0) {
   console.log("Sorry- Grant Wins!");
 } else {
   console.log("You Won!");
 }
 
-}
+let playAgain= prompt("Would you like to 'attack' or 'quit'?");
+  nextRound:
+    if (playAgain === 'quit') {
+      break nextRound;
+    } else{
+      startCombat();
+      console.log(startCombat());
+    }
 
-if (playQuestion === "Yes" || playQuestion === "Yeah" || playQuestion === "yes" || playQuestion === "yeah") {
-  randomGame();
+break;
+
+}
+}
+}
 }
 
